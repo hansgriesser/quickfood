4b51b9e87ad871d35be853af7f1c0027856233ea
Michael Krametter <krametter38@gmail.com>
2026-01-10 20:41:26 +0100

    fix admin-module

--- apps/web/src/app/features/admin/admin-routing-module.ts
+++ apps/web/src/app/features/admin/admin-routing-module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminOverviewComponent } from './pages/overview/admin-overview.component';
-import { AdminRestaurantsComponent } from './pages/restaurants/admin-restaurants.components';
+import { AdminRestaurantsComponent } from './pages/restaurants/admin-restaurants.component';
+
 
 const routes: Routes = [
   { path: '', component: AdminOverviewComponent },
-  { path: 'restaurants', component: AdminRestaurantsComponent },
+  { path: 'restaurants', component: AdminRestaurantsComponent }
 ];
 
 @NgModule({
