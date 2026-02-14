5eaeb431d3e625cfa50d2359915d46425d12c763
Hans <hans.griesser@gmail.com>
2026-02-14 18:48:51 +0100

    fix: create restaurant bugfix

--- apps/api/src/owner/restaurants/dto/create-owner-restaurant.dto.ts
+++ apps/api/src/owner/restaurants/dto/create-owner-restaurant.dto.ts
@@ -1,6 +1,18 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateOwnerRestaurantDto {
+  @IsString()
   name: string;
+
+  @IsOptional()
+  @IsString()
   contactEmail?: string;
+
+  @IsOptional()
+  @IsString()
   contactPhone?: string;
+
+  @IsOptional()
+  @IsString()
   category?: string;
 }
--- apps/api/src/owner/restaurants/dto/update-owner-restaurant.dto.ts
+++ apps/api/src/owner/restaurants/dto/update-owner-restaurant.dto.ts
@@ -1,9 +1,29 @@
+import { IsArray, IsOptional, IsString } from 'class-validator';
+
 export class UpdateOwnerRestaurantDto {
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsString()
   contactEmail?: string;
+
+  @IsOptional()
+  @IsString()
   contactPhone?: string;
+
+  @IsOptional()
+  @IsString()
   category?: string;
+
+  @IsOptional()
+  @IsArray()
   openingHours?: OwnerOpeningHourInput[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
   deliveryZoneIds?: string[];
 }
 
